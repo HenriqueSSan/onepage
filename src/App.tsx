d892678f24141ba76b0a7c83006b87f6cd6b8a8b
HenriqueSSan <hsstudentwork@gmail.com>
2023-01-19 03:05:06 -0300

    feat(hero): finished hero section and styles for large,medium,small devices

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import illustrationImg from './assets/illustration.svg';
 export function App() {
   return (
     <main className="l-main">
-      <section className="hero">
-        <div className="hero__container d-flex align-items-center justify-between">
+      <section className="hero bg-gray-200">
+        <div className="hero__container container d-flex align-items-center justify-between flex-dir-col">
           <div className="hero__texts">
-            <h1 className="hero__title">Lorem ipsum dolor sit amet</h1>
+            <h1 className="hero__title font-family-200 weight-700 clr-gray-400">
+              Lorem ipsum dolor <br />
+              sit amet
+            </h1>
 
-            <p className="hero__description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu massa
-              sit amet augue consectetur ullamcorper non ac massa.
+            <p className="hero__description font-family-100 weight-400 clr-gray-300">
+              Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Maecenas eu
+              massa sit amet <br /> augue consectetur ullamcorper non ac massa.
             </p>
 
-            <button className="btn" type="button" data-primary>
+            <button className="btn font-family-100 weight-700" type="button" data-primary>
               Botão
             </button>
           </div>
