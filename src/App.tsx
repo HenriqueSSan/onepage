072fed4368ea7ae642f327f9a1cc946654173ccc
HenriqueSSan <hsstudentwork@gmail.com>
2023-01-18 22:09:43 -0300

    feat(workspace): created initial styles in workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import './styles/index.scss';
 
+import illustrationImg from './assets/illustration.svg';
+
 export function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main className="l-main">
+      <section className="hero">
+        <div className="hero__container d-flex align-items-center justify-between">
+          <div className="hero__texts">
+            <h1 className="hero__title">Lorem ipsum dolor sit amet</h1>
+
+            <p className="hero__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu massa
+              sit amet augue consectetur ullamcorper non ac massa.
+            </p>
+
+            <button className="btn" type="button" data-primary>
+              Botão
+            </button>
+          </div>
+
+          <figure className="hero__figure">
+            <img
+              className="hero__image"
+              src={illustrationImg}
+              alt="Ilustração no inicio da página"
+            />
+          </figure>
+        </div>
+      </section>
+    </main>
   );
 }
